6fbb14568331503471b48ea56fc7c35e0689fdb4
Tiramisu <135003824+Se7enseads@users.noreply.github.com>
2026-01-16 18:15:46 +0300

    :sparkles: parse and display the json data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,99 @@
-fn fetch_github_activity(username: &String) -> Result<(), String> {
+#[derive(serde::Deserialize, Debug)]
+struct Event {
+    r#type: EventType,
+    payload: Payload,
+    repo: Repo,
+    created_at: String,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct Payload {
+    action: Option<String>,
+    issue: Option<Issue>,
+    head: Option<String>,
+    before: Option<String>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct Repo {
+    name: String,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct Issue {
+    url: String,
+}
+
+#[derive(serde::Deserialize, Debug)]
+enum EventType {
+    CommitCommentEvent,
+    CreateEvent,
+    DeleteEvent,
+    DiscussionEvent,
+    ForkEvent,
+    GollumEvent,
+    IssueCommentEvent,
+    IssuesEvent,
+    MemberEvent,
+    PublicEvent,
+    PullRequestEvent,
+    PullRequestReviewEvent,
+    PullRequestReviewCommentEvent,
+    PushEvent,
+    ReleaseEvent,
+    WatchEvent,
+}
+
+fn display_user_activity(events: &Vec<Event>) {
+    for event in events {
+        match event.r#type {
+            EventType::CreateEvent => {
+                println!("Created {}", event.repo.name);
+            }
+            EventType::DeleteEvent => {
+                println!("Deleted {}", event.repo.name);
+            }
+            EventType::ForkEvent => {
+                println!("Forked {}", event.repo.name);
+            }
+            EventType::WatchEvent => {
+                println!("Starred {}", event.repo.name);
+            }
+            EventType::IssueCommentEvent => {
+                if let Some(issue) = &event.payload.issue {
+                    println!("Commented on {}", issue.url);
+                }
+            }
+            EventType::IssuesEvent => {
+                if let Some(action) = &event.payload.action {
+                    println!("{} issue on {}", action, event.repo.name);
+                }
+            }
+            EventType::PullRequestEvent => {
+                if let Some(action) = &event.payload.action {
+                    println!("{} pull request on {}", action, event.repo.name);
+                }
+            }
+            EventType::CommitCommentEvent => {
+                if let Some(action) = &event.payload.action {
+                    println!("{} {}", action, event.repo.name);
+                }
+            }
+
+            _ => {
+                println!(
+                    "[{:?}] on {} at {} {}",
+                    event.r#type,
+                    event.repo.name,
+                    event.created_at,
+                    event.payload.action.as_deref().unwrap_or("")
+                );
+            }
+        }
+    }
+}
+
+fn fetch_github_activity(username: &String) -> Result<Vec<Event>, String> {
     // Check if username is empty
     if username.is_empty() {
         return Err("Username cannot be empty".to_string());
@@ -17,18 +112,34 @@ fn fetch_github_activity(username: &String) -> Result<(), String> {
         .send()
         .map_err(|e| format!("Failed to send request: {e}"))?;
 
-    // TODO: Parse and format the JSON response for better readability
-    let body = response
-        .text()
-        .map_err(|e| format!("Failed to read response body: {e}"))?;
 
-    // Display the fetched activity
-    println!("GitHub Activity for {username}:\n {body}");
-    Ok(())
+    // Return error if response status is not success
+    if !response.status().is_success() {
+        return Err(format!(
+            "Failed to fetch github activity: HTTP {}",
+            response.status()
+        ));
+    }
+
+    // Return the JSON response
+    let json = response
+        .json::<Vec<Event>>()
+        .map_err(|e| format!("Failed to read response body: {e}"));
+    json
 }
 
-fn main() -> Result<(), String> {
+fn main() {
     // Get username from command line arguments
     let args = std::env::args().nth(1).unwrap_or_default();
-    fetch_github_activity(&args)
+    let username = args.trim().to_string();
+
+    match fetch_github_activity(&username) {
+        Ok(events) => {
+            display_user_activity(&events);
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        }
+    }
 }
